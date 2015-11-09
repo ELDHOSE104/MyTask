d50f24fb1716f26fababb6935ab3e5e602a497a2
Eldhose <eldhose@qburst.com>
2015-11-09 17:59:03 +0530

    last commit

--- ajax/db.php
+++ ajax/db.php
@@ -1,7 +1,7 @@
 <?php 
-$DB_HOST = 'localhost';
-$DB_USER = 'root';
-$DB_PASS = 'qburst';
-$DB_NAME = 'angularcode_task';
+$DB_HOST = 'mysql.hostinger.in';
+$DB_USER = 'u679683804_eldho';
+$DB_PASS = 'eldhose4444';
+$DB_NAME = 'u679683804_todo';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
