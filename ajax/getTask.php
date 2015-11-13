fc18ba221a49142a890d1076e37b7e47d74136b2
Eldhose <eldhose@qburst.com>
2015-11-13 17:58:54 +0530

    last commit as of nov 13

--- ajax/getTask.php
+++ ajax/getTask.php
@@ -11,8 +11,8 @@ $arr = array();
 if($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
 		$arr[] = $row;	
-	}
-}
+										 }
+						  }
 
 # JSON-encode the response
 echo $json_response = json_encode($arr);
--- validate.php
+++ validate.php
@@ -1,96 +1,25 @@
-
 <?php
-/*** begin our session ***/
 session_start();
-
-/*** first check that both the username, password and form token have been sent ***/
-if(!isset( $_POST['phpro_username'], $_POST['phpro_password']))
+require_once 'ajax/db.php';
+$email =  $_POST['phpro_username'];
+$password = $_POST['phpro_password'];
+//echo $email;
+//echo $password;
+if(isset($_POST['submit']))
 {
-    $message = 'Please enter a valid username and password';
-}
-/*** check the username is the correct length ***/
-elseif (strlen( $_POST['phpro_username']) > 20 || strlen($_POST['phpro_username']) < 4)
-{
-    $message = 'Incorrect Length for Username';
-}
-/*** check the password is the correct length ***/
-elseif (strlen( $_POST['phpro_password']) > 20 || strlen($_POST['phpro_password']) < 4)
-{
-    $message = 'Incorrect Length for Password';
-}
-/*** check the password has only alpha numeric characters ***/
-elseif (ctype_alnum($_POST['phpro_password']) != true)
-{
-        /*** if there is no match ***/
-        $message = "Password must be alpha numeric";
-}
-else
-{
-    /*** if we are here the data is valid and we can insert it into database ***/
-    $phpro_username = filter_var($_POST['phpro_username'], FILTER_SANITIZE_STRING);
-    $phpro_password = filter_var($_POST['phpro_password'], FILTER_SANITIZE_STRING);
+$query = "select * from user where email like '$email'";
+//$query="insert into user values('','$email','$password')";
+$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
 
-    /*** now we can encrypt the password ***/
-    $phpro_password = sha1( $phpro_password );
-    
-    /*** connect to database ***/
-    /*** mysql hostname ***/
-    $mysql_hostname = 'localhost';
+$arr = array();
+if($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$arr[] = $row;
+										 }
 
-    /*** mysql username ***/
-    $mysql_username = 'root';
+						  }
 
-    /*** mysql password ***/
-    $mysql_password = 'qburst';
 
-    /*** database name ***/
-    $mysql_dbname = 'angularcode_task';
 
-    try
-    {
-        $dbh = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_dbname", $mysql_username, $mysql_password);
-        /*** $message = a message saying we have connected ***/
-
-        /*** set the error mode to excptions ***/
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        /*** prepare the insert ***/
-        $stmt = $dbh->prepare("INSERT INTO phpro_users (phpro_username, phpro_password ) VALUES (:phpro_username, :phpro_password )");
-
-        /*** bind the parameters ***/
-        $stmt->bindParam(':phpro_username', $phpro_username, PDO::PARAM_STR);
-        $stmt->bindParam(':phpro_password', $phpro_password, PDO::PARAM_STR, 40);
-
-        /*** execute the prepared statement ***/
-        $stmt->execute();
-
-        /*** unset the form token session variable ***/
-        unset( $_SESSION['form_token'] );
-
-        /*** if all is done, say thanks ***/
-        $message = 'New user added';
-    }
-    catch(Exception $e)
-    {
-        /*** check if the username already exists ***/
-        if( $e->getCode() == 23000)
-        {
-            $message = 'Username already exists';
-        }
-        else
-        {
-            /*** if we are here, something has gone wrong with the database ***/
-            $message = 'We are unable to process your request. Please try again later"';
-        }
-    }
 }
 ?>
-
-<html>
-<head>
-<title>Validate</title>
-</head>
-<body>
-<p><?php echo $message; ?>
-</body>
-</html>
