3f38ba8132fb07d7d21d8bf93e2704a6a8a921cc
ELDHOSE <eldhose104@gmail.com>
2015-11-20 00:08:48 +0530

    [deploy: production]

--- ajax/db.php
+++ ajax/db.php
@@ -3,5 +3,11 @@ $DB_HOST = 'mysql.hostinger.in';
 $DB_USER = 'u679683804_eldho';
 $DB_PASS = 'eldhose4444';
 $DB_NAME = 'u679683804_todo';
+
+/*login with google keys*/
+$client_id = '492769946544-ktjauuhqughrgre7blu2b6cmuuir1688.apps.googleusercontent.com';
+$client_secret = 'xUqeCQ7pj42axoJ5E1iVu0kyr';
+$redirect_uri = 'http://localhost/MyTask/validate.php';
+
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
