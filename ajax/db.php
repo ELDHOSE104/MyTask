40b97d4752eaae35f2ee89a57ffca476d2dfdab5
Eldhose <eldhose@qburst.com>
2015-11-20 09:54:50 +0530

    fixed local db.php

--- ajax/db.php
+++ ajax/db.php
@@ -3,5 +3,11 @@ $DB_HOST = 'localhost';
 $DB_USER = 'root';
 $DB_PASS = 'qburst';
 $DB_NAME = 'angularcode_task';
+
+/*login with google keys*/
+$client_id = '492769946544-ktjauuhqughrgre7blu2b6cmuuir1688.apps.googleusercontent.com';
+$client_secret = 'xUqeCQ7pj42axoJ5E1iVu0kyr';
+$redirect_uri = 'http://localhost/MyTask/index.php';
+
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
