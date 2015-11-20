cd3f0851752ca11f845815854c6f104c0bd1b706
Eldhose <eldhose@qburst.com>
2015-11-20 09:52:22 +0530

    changed db.php

--- ajax/db.php
+++ ajax/db.php
@@ -7,7 +7,7 @@ $DB_NAME = 'u679683804_todo';
 /*login with google keys*/
 $client_id = '492769946544-ktjauuhqughrgre7blu2b6cmuuir1688.apps.googleusercontent.com';
 $client_secret = 'xUqeCQ7pj42axoJ5E1iVu0kyr';
-$redirect_uri = 'http://localhost/MyTask/validate.php';
+$redirect_uri = 'http://eldhose.xyz/index.php';
 
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
